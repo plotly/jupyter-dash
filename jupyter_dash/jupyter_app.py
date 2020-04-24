891a7c628f6c5a5fb3e04f76a1839dbf38a2a526
Jon Mease <jon.mease@gmail.com>
2020-04-24 19:46:18 -0400

    Add _alive endpoint to check when server is running, support iframe

--- jupyter_dash/jupyter_app.py
+++ jupyter_dash/jupyter_app.py
@@ -6,7 +6,7 @@ import flask.cli
 from threading import Thread
 from retrying import retry
 
-from IPython.display import HTML, display
+from IPython.display import HTML, IFrame, display
 
 
 class JupyterDash(dash.Dash):
@@ -23,11 +23,8 @@ class JupyterDash(dash.Dash):
 
         self.base_url = base_url
 
-        # Register custom route to shut down server
-        self._instance_shutdown_route = '/_shutdown'
-        shutdown_route = self._instance_shutdown_route
-
-        @self.server.route(shutdown_route, methods=['GET'])
+        # Register route to shut down server
+        @self.server.route('/_shutdown', methods=['GET'])
         def shutdown():
             func = request.environ.get('werkzeug.server.shutdown')
             if func is None:
@@ -35,10 +32,16 @@ class JupyterDash(dash.Dash):
             func()
             return 'Server shutting down...'
 
+        # Register route that we can use to poll to see when server is running
+        @self.server.route('/_alive', methods=['GET'])
+        def alive():
+            return 'Alive'
+
         self.server.logger.disabled = True
 
     def run_server(
             self,
+            inline=False, width=800, height=650,
             host=os.getenv("HOST", "127.0.0.1"),
             port=os.getenv("PORT", "8050"),
             **kwargs
@@ -66,9 +69,6 @@ class JupyterDash(dash.Dash):
         dashboard_url = "{base}{requests_pathname_prefix}".format(
             base=base, requests_pathname_prefix=requests_pathname_prefix
         )
-
-        # Store new shutdown route
-        JupyterDash._shutdown_route[port] = self._instance_shutdown_route
 
         # Enable supported dev tools by default
         for k in [
@@ -100,23 +100,40 @@ class JupyterDash(dash.Dash):
         thread.setDaemon(True)
         thread.start()
 
-        # Display a hyperlink that can be clicked to open Dashboard
-        display(HTML(
-            "Dash app running on <a href='{dashboard_url}' "
-            "target='_blank'>{dashboard_url}</a>".format(
-                dashboard_url=dashboard_url
-            )
-        ))
+        # Wait for server to start up
+        alive_url = "http://{host}:{port}/_alive".format(
+            host=host, port=port
+        )
+
+        # Wait for app to respond to _alive endpoint
+        @retry(
+            stop_max_attempt_number=15,
+            wait_exponential_multiplier=10,
+            wait_exponential_max=1000
+        )
+        def wait_for_app():
+            res = requests.get(alive_url)
+            return res.content.decode()
+
+        wait_for_app()
+
+        if inline:
+            display(IFrame(dashboard_url, width, height))
+        else:
+            # Display a hyperlink that can be clicked to open Dashboard
+            display(HTML(
+                "Dash app running on <a href='{dashboard_url}' "
+                "target='_blank'>{dashboard_url}</a>".format(
+                    dashboard_url=dashboard_url
+                )
+            ))
 
     @classmethod
     def _terminate_server_for_port(cls, host, port):
-        port = str(port)
-        if port in cls._shutdown_route:
-            shutdown_route = cls._shutdown_route.pop(port)
-            shutdown_url = "http://{host}:{port}{shutdown_route}".format(
-                host=host, port=port, shutdown_route=shutdown_route
-            )
-            try:
-                response = requests.get(shutdown_url)
-            except Exception as e:
-                pass
+        shutdown_url = "http://{host}:{port}/_shutdown".format(
+            host=host, port=port
+        )
+        try:
+            response = requests.get(shutdown_url)
+        except Exception as e:
+            pass

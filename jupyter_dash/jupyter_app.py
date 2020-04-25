dc8e23af82b6cf5d947315aa9427f249e75667ee
Jon Mease <jon.mease@gmail.com>
2020-04-25 08:16:20 -0400

    Add JupyterLab extension support.
    There are now three display modes, controled by `mode` argument to `run_server()`
     - 'external': Output hyperlink to click to open app in browser tab
     - 'inline': Output in iframe inline in notebook
     - 'jupyterlab': Output to JupyterLab tab.

--- jupyter_dash/jupyter_app.py
+++ jupyter_dash/jupyter_app.py
@@ -6,19 +6,33 @@ import flask.cli
 from threading import Thread
 from retrying import retry
 
+import IPython
 from IPython.display import HTML, IFrame, display
-
+from ipykernel.comm import Comm
 
 class JupyterDash(dash.Dash):
-    _shutdown_route = {}
+    _dash_comm = Comm(target_name='dash_viewer')
+    _jupyterlab_base_url = None
 
     def __init__(self, base_url=None, **kwargs):
+        # Infer requests_pathname_prefix
+        requests_pathname_prefix = kwargs.get('requests_pathname_prefix', None)
+        if requests_pathname_prefix is None:
+            if JupyterDash._jupyterlab_base_url:
+                # We're in a JupyterLab context and jupyter_server_proxy is installed
+                kwargs['requests_pathname_prefix'] = '/proxy/{port}/'
+
+        # Call superclass constructor
         super(JupyterDash, self).__init__(**kwargs)
 
-        # Check PLOTLY_DASH_DOMAIN_BASE
+        # Infer base_url
         if base_url is None:
             domain_base = os.environ.get('PLOTLY_DASH_DOMAIN_BASE', None)
-            if domain_base:
+            if JupyterDash._jupyterlab_base_url:
+                # JupyterLab extension reported base url
+                base_url = JupyterDash._jupyterlab_base_url
+            elif domain_base:
+                # Dash Enterprise set PLOTLY_DASH_DOMAIN_BASE environment variable
                 base_url = 'https://' + domain_base
 
         self.base_url = base_url
@@ -41,11 +55,38 @@ class JupyterDash(dash.Dash):
 
     def run_server(
             self,
-            inline=False, width=800, height=650,
+            mode=None, width=800, height=650,
             host=os.getenv("HOST", "127.0.0.1"),
             port=os.getenv("PORT", "8050"),
             **kwargs
     ):
+        # Validate / infer display
+        valid_display_values = ["jupyterlab", "inline", "external"]
+
+        if mode is None:
+            # Infer default display argument
+            if JupyterDash._jupyterlab_base_url:
+                # There is an active JupyterLab extension
+                mode = "jupyterlab"
+            else:
+                mode = "external"
+        elif not isinstance(mode, str):
+            raise ValueError(
+                "The display argument must be a string\n"
+                "    Received value of type {typ}: {val}".format(
+                    typ=type(mode), val=repr(mode)
+                )
+            )
+        else:
+            mode = mode.lower()
+            if mode not in valid_display_values:
+                raise ValueError(
+                    "Invalid display argument {display}\n"
+                    "    Valid arguments: {valid_display_values}".format(
+                        display=repr(mode), valid_display_values=valid_display_values
+                    )
+                )
+
         # Terminate any existing server using this port
         self._terminate_server_for_port(host, port)
 
@@ -64,7 +105,7 @@ class JupyterDash(dash.Dash):
         if self.base_url is None:
             base = f'http://{host}:{port}'
         else:
-            base = self.base_url
+            base = self.base_url.rstrip('/')
 
         dashboard_url = "{base}{requests_pathname_prefix}".format(
             base=base, requests_pathname_prefix=requests_pathname_prefix
@@ -117,16 +158,29 @@ class JupyterDash(dash.Dash):
 
         wait_for_app()
 
-        if inline:
+        if mode == 'inline':
             display(IFrame(dashboard_url, width, height))
-        else:
+        elif mode == 'external':
             # Display a hyperlink that can be clicked to open Dashboard
-            display(HTML(
-                "Dash app running on <a href='{dashboard_url}' "
-                "target='_blank'>{dashboard_url}</a>".format(
-                    dashboard_url=dashboard_url
-                )
+            print("Dash app running on {dashboard_url}".format(
+                dashboard_url=dashboard_url
             ))
+        elif mode == 'jupyterlab':
+            if not self._jupyterlab_base_url:
+                raise IOError("""
+Unable to communicate with the jupyterlab-dash JupyterLab extension.
+Is this Python kernel running inside JupyterLab with the jupyterlab-dash
+extension installed?
+
+You can install the extension with:
+
+$ jupyter labextension install jupyterlab-dash
+""")
+            # Update front-end extension
+            self._dash_comm.send({
+                'type': 'show',
+                'port': port
+            })
 
     @classmethod
     def _terminate_server_for_port(cls, host, port):
@@ -137,3 +191,22 @@ class JupyterDash(dash.Dash):
             response = requests.get(shutdown_url)
         except Exception as e:
             pass
+
+
+# Register handler to process events sent from the
+# front-end JupyterLab extension to the python kernel
+@JupyterDash._dash_comm.on_msg
+def _receive_message(msg):
+    msg_data = msg.get('content').get('data')
+    msg_type = msg_data.get('type', None)
+    if msg_type == 'base_url_response':
+        JupyterDash._jupyterlab_base_url = msg_data['base_url']
+
+
+# If running in an ipython kernel,
+# request that the front end extension send us the notebook server base URL
+if IPython.get_ipython() is not None:
+    if JupyterDash._dash_comm.kernel is not None:
+        JupyterDash._dash_comm.send({
+            'type': 'base_url_request'
+        })

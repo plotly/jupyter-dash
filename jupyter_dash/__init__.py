55b97c42b69524f749bced59f050ff3cbc1513dd
Jon Mease <jon.mease@gmail.com>
2020-04-29 07:58:10 -0400

    Automatically install and enable the classic notebook extension during package installation

--- jupyter_dash/__init__.py
+++ jupyter_dash/__init__.py
@@ -1,1 +1,13 @@
+import jupyter_dash.comms
 from .jupyter_app import JupyterDash
+
+
+def _jupyter_nbextension_paths():
+    return [
+        {
+            "section": "notebook",
+            "src": "nbextension/static",
+            "dest": "jupyter_dash",
+            "require": "jupyter_dash/main",
+        }
+    ]
--- setup.py
+++ setup.py
@@ -5,5 +5,17 @@ setup(
     version='0.0.1a1',
     description="Dash support for the Jupyter notebook interface",
     author='Plotly',
-    packages=['jupyter_dash']
+    packages=['jupyter_dash'],
+    install_requires=['dash', 'requests', 'flask', 'retrying', 'ipython'],
+    include_package_data=True,
+    data_files=[
+        # like `jupyter nbextension install --sys-prefix`
+        ("share/jupyter/nbextensions/jupyter_dash", [
+            "jupyter_dash/nbextension/static/main.js",
+        ]),
+         # like `jupyter nbextension enable --sys-prefix`
+        ("etc/jupyter/nbconfig/notebook.d", [
+            "jupyter_dash/nbextension/jupyter_dash.json"
+        ]),
+    ]
 )

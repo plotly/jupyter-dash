5cb371e01c50561e4180a7cf167e5c1e189ba655
Jon Mease <jon.mease@gmail.com>
2020-04-29 13:13:00 -0400

    Make sure kernel is ready before registering comm in classic notebook

--- jupyter_dash/nbextension/main.js
+++ jupyter_dash/nbextension/main.js
@@ -6,32 +6,32 @@ define([
 ], function(Jupyter, utils){
 
     function load_ipython_extension(){
-        console.info('this is my first extension');
-
         var notebookUrl = window.location.href
         var baseUrl = utils.get_body_data('baseUrl');
         var baseNotebooks = baseUrl + "notebooks"
         var n = notebookUrl.search(baseNotebooks)
         var jupyterServerUrl = notebookUrl.slice(0, n)
-        console.log(jupyterServerUrl)
-        console.log(baseUrl)
 
-        Jupyter.notebook.kernel.comm_manager.register_target('jupyter_dash',
-        function(comm, msg) {
-            // Register handlers for later messages:
-            comm.on_msg(function(msg) {
-                var msgData = msg.content.data;
-                console.log(msgData)
-                if (msgData.type === 'base_url_request') {
-                    comm.send({
-                        type: 'base_url_response',
-                        server_url: jupyterServerUrl,
-                        base_subpath: baseUrl,
-                        frontend: "notebook"
+        var register_comm = function() {
+            Jupyter.notebook.kernel.comm_manager.register_target('jupyter_dash',
+                function (comm, msg) {
+                    // Register handlers for later messages:
+                    comm.on_msg(function (msg) {
+                        var msgData = msg.content.data;
+                        console.log(msgData)
+                        if (msgData.type === 'base_url_request') {
+                            comm.send({
+                                type: 'base_url_response',
+                                server_url: jupyterServerUrl,
+                                base_subpath: baseUrl,
+                                frontend: "notebook"
+                            });
+                        }
                     });
-                }
-            });
-        });
+                });
+        };
+
+        Jupyter.notebook.events.on('kernel_ready.Kernel', register_comm)
     }
 
     return {
